0b51ceec20de05962dd8e21d47f347df76fe0f2b
elshora97 <A.elshora@dataserve.com.sa>
2024-08-19 12:18:12 +0300

    connected to DB

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('E-Commerce API');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () =>
+      console.log(`app is up and running on port: ${PORT}`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
