a6a62f3b1facd46e0f300e27a4394f1817395167
elshora97 <A.elshora@dataserve.com.sa>
2024-08-28 20:25:09 +0300

    some fixes

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const fileupload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
 
 //database
 const connectDB = require("./db/connect");
@@ -26,16 +30,24 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 const PORT = process.env.PORT || 3000;
 
-app.use(morgan("tiny"));
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(cors());
 app.use(express.static("./puplic"));
 app.use(fileupload());
 
-app.get("/", (req, res) => {
-  res.send("E-commerce api");
-});
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/products", productRouter);
